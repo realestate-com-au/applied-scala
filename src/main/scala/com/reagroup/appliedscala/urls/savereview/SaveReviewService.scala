ccbea0c965605f5e40d4afe3a81ca93a137fed5b
stilianosmenelaou <stilianos.menelaou@rea-group.com>
2020-11-06 16:35:17 +1100

    Save review works

--- src/main/scala/com/reagroup/appliedscala/urls/savereview/SaveReviewService.scala
+++ src/main/scala/com/reagroup/appliedscala/urls/savereview/SaveReviewService.scala
@@ -1,5 +1,6 @@
 package com.reagroup.appliedscala.urls.savereview
 
+import cats.data.Validated.{Invalid, Valid}
 import cats.data.{NonEmptyList, Validated, ValidatedNel}
 import cats.effect.IO
 import cats.implicits._
@@ -8,17 +9,29 @@ import com.reagroup.appliedscala.models.{Movie, MovieId}
 class SaveReviewService(saveReview: (MovieId, ValidatedReview) => IO[ReviewId],
                         fetchMovie: MovieId => IO[Option[Movie]]) {
 
-  /**
-    * Before saving a `NewReviewRequest`, we want to check that the movie exists and then
-    * validate the request in order to get a `ValidatedReview`.
-    * Complete `NewReviewValidator`, then use it here before calling `saveReview`.
-    * Return all errors encountered whether the movie exists or not.
-    *
-    */
-  def save(movieId: MovieId, review: NewReviewRequest): IO[ValidatedNel[ReviewValidationError, ReviewId]] = ???
+  def save(movieId: MovieId, review: NewReviewRequest): IO[ValidatedNel[ReviewValidationError, ReviewId]] = {
+    for {
+      maybeFetchedMovie <- fetchMovie(movieId)
+      maybeValidatedMovie = validateMovie(maybeFetchedMovie)
+      maybeValidatedReview = validateReview(review)
+      combinedValidatedReview = maybeValidatedMovie.productR(maybeValidatedReview)
+      reviewId <- combinedValidatedReview.traverse(validatedReview => saveReview(movieId, validatedReview))
+//      reviewId = combinedValidatedReview.map(validatedReview => saveReview(movieId, validatedReview)) match {
+//        case Valid(ioReview) => ioReview.map(reviewId => Valid(reviewId))
+//        case Invalid(error) => IO(Invalid(error))
+//      }
+    } yield reviewId
+  }
 
-  private def validateMovie(maybeMovie: Option[Movie]): ValidatedNel[ReviewValidationError, Movie] = ???
+  private def validateMovie(maybeMovie: Option[Movie]): ValidatedNel[ReviewValidationError, Movie] = {
+    maybeMovie match {
+      case Some(movie) => movie.valid
+      case None => MovieDoesNotExist.invalidNel
+    }
+  }
 
-  private def validateReview(review: NewReviewRequest): ValidatedNel[ReviewValidationError, ValidatedReview] = ???
+  private def validateReview(review: NewReviewRequest): ValidatedNel[ReviewValidationError, ValidatedReview] = {
+    NewReviewValidator.validate(review)
+  }
 
 }
